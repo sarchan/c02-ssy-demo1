3eb857fab7347e94e508726a21fb46be4d0b489a
Arno Hollosi <arno.hollosi@campus02.at>
2018-09-21 13:19:34 +0200

    Package updates, remove templating, add stacktrace to console output

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
-const index = require('./routes/index');
-
 const app = express();
 
-// Template engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+const index = require('./routes/index');
 
 // Generic application setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
@@ -24,20 +17,15 @@ app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404);
+  res.send('Not found');
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  console.log(err.stack);
+  res.send(err.stack);
 });
 
 module.exports = app;
