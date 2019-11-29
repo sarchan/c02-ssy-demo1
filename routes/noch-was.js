4ae7e6846558e20f0e9fcc42966f4ef6fff27f59
Arno Hollosi <arno.hollosi@campus02.at>
2019-11-29 17:40:43 +0100

    weitere Funktionen mit Parameter

--- routes/noch-was.js
+++ routes/noch-was.js
@@ -7,4 +7,11 @@ function warum(request, response) {
     response.send('Was funktioniert hier warum?');
 }
 
+router.get('/mehr', mehr);
+
+function mehr(req, res) {
+    const schoko = req.query.schoko;
+    res.send('Will Schoko: ' + schoko);
+}
+
 module.exports = router;
