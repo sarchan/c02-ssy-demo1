8e6bdf2a23531ef28b30a0455d52eb572ce213d4
Arno Hollosi <arno.hollosi@campus02.at>
2020-05-29 17:20:01 +0200

    package updates, app.js vereinfacht

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
-const app = express();
-
-const index = require('./routes/index');
 
 // Generic application setup
+const app = express();
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Load routes into variables
+const index = require('./routes/index');
 
 // Routes
 app.use('/', index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.status(404);
-  res.send('Not found');
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  console.log(err.stack);
-  res.send(err.stack);
-});
 
 module.exports = app;
