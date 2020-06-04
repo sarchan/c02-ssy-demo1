05cab4d7748e00fb2e8f19c967e43a91738cd19d
Arno Hollosi <arno.hollosi@campus02.at>
2020-06-04 15:40:07 +0200

    /calc-Route angelegt

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ app.use(express.urlencoded({extended: false}));
 // Load routes into variables
 const index = require('./routes/index');
 const noch = require('./routes/noch');
+const calcRouter = require('./routes/calc');
 
 // Routes
 app.use('/', index);
 app.use('/noch', noch);
+app.use('/calc', calcRouter);
 
 module.exports = app;
---
+++ routes/calc.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/add', plus);
+router.get('/sub', minus);
+router.get('/mul', multiply);
+router.get('/div', divide);
+
+function plus(req, res) {
+    const zahl1 = parseFloat(req.query.number1);
+    const zahl2 = parseFloat(req.query.number2);
+    const result = zahl1 + zahl2;
+    res.send('Result: ' + result);
+}
+
+function minus(req, res) {
+    const zahl1 = parseFloat(req.query.number1);
+    const zahl2 = parseFloat(req.query.number2);
+    const result = zahl1 - zahl2;
+    res.send('Result: ' + result);
+}
+
+function multiply(req, res) {
+    const zahl1 = parseFloat(req.query.number1);
+    const zahl2 = parseFloat(req.query.number2);
+    const result = zahl1 * zahl2;
+    res.send('Result: ' + result);
+}
+
+function divide(req, res) {
+    const zahl1 = parseFloat(req.query.number1);
+    const zahl2 = parseFloat(req.query.number2);
+    const result = zahl1 / zahl2;
+    res.send('Result: ' + result);
+}
+
+module.exports = router;
